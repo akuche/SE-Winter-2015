7606ccb47e0c94a8c1545b7f8b01297bc362efba
aubin <aubinosfr@yahoo.fr>
2015-10-28 11:52:42 +0100

    fix file run.sh

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@ gcc -w -m32 -o selfie selfie.c
 touch out
 ./selfie -c < cStarTest.c
 mv out cStarTest.mips
-./testSelfie -m 64 cStarTest.mips < cStarTest.c
+./selfie -m 64 cStarTest.mips < cStarTest.c
 
