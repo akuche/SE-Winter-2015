69176e0a0cd8330f134a0329d77b9fbde26c339b
aubin <aubinosfr@yahoo.fr>
2015-10-29 11:07:26 +0100

    yield_scheduling suite

--- runSelfComp.sh
+++ runSelfComp.sh
@@ -4,6 +4,6 @@ gcc -w -m32 -o selfie selfie.c
  ./selfie -c < selfie.c
  mv out selfie.mips1
  touch out
- ./selfie -m 32 selfie.mips1 < selfie.c
+ ./selfie -m 64 selfie.mips1 < selfie.c
  mv out selfie.mips2
  diff -s selfie.mips1 selfie.mips2
